133b04afa6e0495c14bfe469941e6669de3ab51e
santhoshkanna1207 <latteri.sandy17@gmail.com>
2023-11-01 10:51:23 +0000

    secod

--- java.java
+++ java.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World haha hellla joker kllllllllllllllllll!"); 
+        System.out.println("Hello, World haha hellla joker "); 
     }
 }
