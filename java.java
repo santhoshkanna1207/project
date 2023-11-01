2fcd3ae8acbf24a69d4f3ae868579058747c5320
santhoshkanna1207 <147807373+santhoshkanna1207@users.noreply.github.com>
2023-11-01 21:26:37 +0530

    Update java.java

--- java.java
+++ java.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, haha hellla joker "); 
+        System.out.println("Hi, haha hellla joker "); 
     }
 }
