9487f064027da6bd3c327fe07ac28b534736dbac
santhoshkanna1207 <latteri.sandy17@gmail.com>
2023-11-01 10:56:55 +0000

    all

--- java1.java
+++ java1.java
@@ -3,7 +3,7 @@ public class First10OddNaturalNum1 {
 	public static void main(String[] args) {
 		
 		System.out.println("The First 10 Odd Natural Numbers are");
-		for(int i = 1; i <= 15; i++) 
+		for(int i = 1; i <= 20; i++) 
 		{
 			System.out.println(2 * i - 1);
 		}
