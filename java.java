dc60d30595d13528baa0a3e6d072f30c1f387ee3
santhosh <latteri.sandy17@gmail.com>
2023-11-06 03:54:11 +0000

    new

--- java.java
+++ java.java
@@ -1,6 +1,6 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hi, joker harlen batman "); 
+        System.out.println("Hi, joker bruce batman "); 
         Hello.hello();
     }
 }
