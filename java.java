8bee0b996f5ce4733111bbca5b50f036f35cf726
santhoshkanna1207 <147807373+santhoshkanna1207@users.noreply.github.com>
2024-11-08 14:54:27 +0530

    Update java.java
    v1

--- java.java
+++ java.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hi, joker bruce captain batman "); 
+        System.out.println("Hi, joker batman "); 
     }
 }
